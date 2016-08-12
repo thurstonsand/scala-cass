1a0fe5b96f411e596f5a93be514f076474304c50
Thurston Sandberg <thurstonsand@gmail.com>
2016-08-12 14:21:04 -0400

    trying a script now...

---
+++ .travis_script.sh
@@ -0,0 +1,23 @@
+mkdir lib
+wget https://github.com/Ichoran/thyme/raw/master/Thyme.jar -O lib/Thyme.jar
+
+cassVersion=""
+if java -version 2>&1 | egrep "java version \"1.7" > /dev/null; then
+    cassVersion="22x"
+else
+    cassVersion="31x"
+fi
+echo "deb http://www.apache.org/dist/cassandra/debian $cassVersion main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+echo "deb-src http://www.apache.org/dist/cassandra/debian $cassVersion main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+
+gpg --keyserver hkp://pgp.mit.edu:80 --recv-keys F758CE318D77295D
+gpg --export --armor F758CE318D77295D | sudo apt-key add -
+
+gpg --keyserver hkp://pgp.mit.edu:80 --recv-keys 2B5C1B00
+gpg --export --armor 2B5C1B00 | sudo apt-key add -
+
+gpg --keyserver hkp://pgp.mit.edu:80 --recv-keys 0353B12C
+gpg --export --armor 0353B12C | sudo apt-key add -
+
+sudo apt-get update -qq
+sudo apt-get install cassandra
