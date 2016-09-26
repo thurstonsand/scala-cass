9b30ce860eeb52462eb5f116c16a64c16e04d587
Thurston Sandberg <thurstonsand@gmail.com>
2016-09-26 17:09:13 -0400

    don't mind me just making the code compile

--- src/main/scala_cass3/com/weather/scalacass/NotRecoverableVersionSpecific.scala
+++ src/main/scala_cass3/com/weather/scalacass/NotRecoverableVersionSpecific.scala
@@ -1,13 +1,13 @@
 package com.weather.scalacass
 
-import com.datastax.driver.core.exceptions.{TransportException, QueryExecutionException, NoHostAvailableException, BusyConnectionException, ConnectionException, DriverInternalError, PagingStateException, QueryExecutionException, UnsupportedFeatureException, UnsupportedProtocolVersionException}
+import com.datastax.driver.core.exceptions.{TransportException, QueryExecutionException, NoHostAvailableException, BusyConnectionException, ConnectionException, DriverInternalError, PagingStateException, UnsupportedFeatureException, UnsupportedProtocolVersionException}
 
 trait NotRecoverableVersionSpecific {
   def apply(t: Throwable) = t match {
     case _: TransportException | _: QueryExecutionException | _: NoHostAvailableException |
-         _: BusyConnectionException | _: ConnectionException | _: DriverInternalError |
-         _: PagingStateException | _: QueryExecutionException | _: UnsupportedFeatureException |
-         _: UnsupportedProtocolVersionException => true
-    case _                                      => false
+      _: BusyConnectionException | _: ConnectionException | _: DriverInternalError |
+      _: PagingStateException | _: UnsupportedFeatureException |
+      _: UnsupportedProtocolVersionException => true
+    case _ => false
   }
 }
