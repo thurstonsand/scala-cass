644773b0cbe7fa7866759b94d5e3252b7cf6f37e
Thurston Sandberg <thurstonsand@gmail.com>
2016-09-27 09:24:05 -0400

    ok...NOW it compiles

--- src/main/scala_cass21/com/weather/scalacass/NotRecoverableVersionSpecific.scala
+++ src/main/scala_cass21/com/weather/scalacass/NotRecoverableVersionSpecific.scala
@@ -3,9 +3,9 @@ package com.weather.scalacass
 import com.datastax.driver.core.exceptions.{NoHostAvailableException, QueryExecutionException, DriverInternalError, PagingStateException, UnsupportedFeatureException}
 
 trait NotRecoverableVersionSpecific {
-  def apply(t: Throwable): Boolean = {
+  def apply(t: Throwable): Boolean = t match {
     case _: NoHostAvailableException | _: QueryExecutionException | _: DriverInternalError | _: PagingStateException |
-         _: UnsupportedOperationException => true
-    case _                                => false
+      _: UnsupportedFeatureException => true
+    case _ => false
   }
 }
