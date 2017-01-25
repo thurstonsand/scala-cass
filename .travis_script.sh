0f18937f590dcbb345d89660494746d3734c74cc
Thurston Sandberg <thurstonsand@gmail.com>
2017-01-25 15:48:29 -0500

    no functions allowed i guess

--- .travis_script.sh
+++ .travis_script.sh
@@ -1,22 +1,15 @@
 #!/usr/bin/env sh
 
-function prepare_thyme {
-    mkdir lib
-    wget https://github.com/Ichoran/thyme/raw/master/Thyme.jar -O lib/Thyme.jar
-}
+mkdir lib
+wget https://github.com/Ichoran/thyme/raw/master/Thyme.jar -O lib/Thyme.jar
 
-function prepare_cassandra {
-    sudo rm /etc/init.d/cassandra /etc/security/limits.d/cassandra.conf
-    cassVersion=""
-    if java -version 2>&1 | egrep "java version \"1.7" > /dev/null; then
-        cassVersion="2.2.7"
-    else
-        cassVersion="3.0.8"
-    fi
+sudo rm /etc/init.d/cassandra /etc/security/limits.d/cassandra.conf
+#cassVersion=""
+#if java -version 2>&1 | egrep "java version \"1.7" > /dev/null; then
+#    cassVersion="2.2.7"
+#else
+#    cassVersion="3.0.8"
+#fi
 
-    wget "http://www.us.apache.org/dist/cassandra/$cassVersion/apache-cassandra-$cassVersion-bin.tar.gz" && tar -xvzf "apache-cassandra-$cassVersion-bin.tar.gz"
-    export PATH="apache-cassandra-$cassVersion/bin:$PATH"
-}
-
-prepare_thyme
-#prepare_cassandra()
+#wget "http://www.us.apache.org/dist/cassandra/$cassVersion/apache-cassandra-$cassVersion-bin.tar.gz" && tar -xvzf "apache-cassandra-$cassVersion-bin.tar.gz"
+#export PATH="apache-cassandra-$cassVersion/bin:$PATH"
